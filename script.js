2d01e65895b003a9fba088b63ecdc29f2b8534e1
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-19 11:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-function indexOfIgnoreCase(s1, s2) {
- // Convert both strings to lowercase for case-insensitive comparison
+function indexOfIgnoreCase(str, subStr) {
   const lowerCaseStr = str.toLowerCase();
   const lowerCaseSubStr = subStr.toLowerCase();
   
-  // Check if lower case str includes lower case subStr
   if (lowerCaseStr.includes(lowerCaseSubStr)) {
-    // If it does, return the index of subStr in str
     return lowerCaseStr.indexOf(lowerCaseSubStr);
   } else {
-    // If not found, return -1
     return -1;
   }
 }
+
+// Please do not change the code below
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(indexOfIgnoreCase(s1, s2));
+
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
